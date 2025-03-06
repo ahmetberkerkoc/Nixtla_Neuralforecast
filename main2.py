db01c5d9ee5a9646ac2c34920c2be6bdb90cc911
ahmet.koc <ahmet.koc@data-boss.com.tr>
2025-03-06 17:11:31 +0300

    informer implmenetation

--- main2.py
+++ main2.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from neuralforecast import NeuralForecast
-from neuralforecast.models import DeepAR
+from neuralforecast.models import DeepAR, Informer, iTransformer, NBEATS, NHITS, TimeMixer
 from neuralforecast.losses.numpy import mse, mae
+from neuralforecast.losses.pytorch import MSE
 
 # Load data
 df = pd.read_csv("data/turkey_gas.csv")
@@ -25,23 +26,42 @@ val_size = int(len(df) * 0.1)
 forecast_horizon = len(test_df)
 
 # Define and train the DeepAR model
-models = [DeepAR(h=forecast_horizon,
-                    input_size=int(1.5)*forecast_horizon,
-                    max_steps=3000,
-                    #scaler_type="standard",
-                    learning_rate=0.00003,
-                    # futr_exog_list=["ABONE_SICAKLIK_HI", "ABONE_SICAKLIK_MIN", "ABONE_RUZGAR_HI", "ABONE_RUZGAR_AVG"]
-                    futr_exog_list= list(train_df.columns[3:-1]),
-                    batch_size=1
-                    # val_size = val_size
-                )]
+models = [
+            # DeepAR(
+            #         h=forecast_horizon,
+            #         input_size=35,
+            #         max_steps=3000,
+            #         #scaler_type="standard",
+            #         learning_rate=0.00003,
+            #         # futr_exog_list=["ABONE_SICAKLIK_HI", "ABONE_SICAKLIK_MIN", "ABONE_RUZGAR_HI", "ABONE_RUZGAR_AVG"]
+            #         futr_exog_list= list(train_df.columns[3:-1]),
+            #         batch_size=1
+            #     ),
+            Informer(
+                 h=forecast_horizon,
+                 input_size=100,
+                 hidden_size = 32,
+                 conv_hidden_size = 64,
+                 n_head = 2,
+                 loss=MSE(),
+                 # futr_exog_list=list(train_df.columns[3:-1]),
+                 scaler_type='identity',
+                 learning_rate=1e-3,
+                 max_steps=1000,
+                 # val_check_steps=50,
+                 # early_stop_patience_steps=2
+                )
+
+
+          ]
+model_name = "Informer"
 nf = NeuralForecast(models=models, freq='D')
 nf.fit(train_df)
 
 # Make predictions for the test period
 forecast_df = nf.predict(futr_df=test_df)
 
-fores = forecast_df['DeepAR']*(10**7)
+fores = forecast_df[model_name]*(10**7)
 fores = fores.to_numpy()
 
 tests = test_df['y']*(10**7)
@@ -55,12 +75,12 @@ print("MAE: {:e}".format(mae(tests, fores)))
 plt.figure(figsize=(12, 6))
 plt.plot(train_df['ds'], train_df['y']*(10**7), label="Train Data", color='blue', linewidth=2)
 plt.plot(test_df['ds'], test_df['y']*(10**7), label="Test Data", color='green', linewidth=2)
-plt.plot(forecast_df['ds'], forecast_df['DeepAR']*(10**7), label="Forecast", color='red', linestyle='dashed', linewidth=2)
+plt.plot(forecast_df['ds'], forecast_df[model_name]*(10**7), label="Forecast", color='red', linestyle='dashed', linewidth=2)
 plt.axvline(test_df['ds'].iloc[0], color='gray', linestyle='--', label='Test Start')
 plt.xlabel("Date")
 plt.ylabel("Gas Consumption (ABONE)")
-plt.title("DeepAR Forecast on Test Set")
+plt.title(f"{model_name} Forecast on Test Set")
 plt.legend()
-plt.savefig("figures/turkey_gas.png")
+plt.savefig(f"figures/turkey_gas_{model_name}.png")
 
 print("control")
